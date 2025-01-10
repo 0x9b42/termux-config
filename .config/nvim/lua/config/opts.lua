dab3de6da438c3968739da83f3b46fa63ce6aeb2
Mob <0x9b42@gmail.com>
2025-01-11 00:55:24 +0700

    update_neovim_config

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -16,4 +16,7 @@ set statusline=%f%m%r\ %=0x%O\ %b\ 0x%B\ %=%l:%c\ %L
 
 command! Smali2Java let @a=expand('%:r') | tabe | execute 'read !smalitojava ' . @a . '.smali' | execute 'edit ' . @a . '.java'
 
+"command! Smali2Java let @a=expand('%:r') | tabnew | !smalitojava shellescape(@a . '.smali') | file shellescape(@a . '.java')
+
+
 ]]
